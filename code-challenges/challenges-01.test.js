2b8eca147698b81c23600b01396e8dc3dfd272b5
Klace Koch <koch.klace@gmail.com>
2021-01-05 14:51:58 -0800

    Fizz buzz done

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -92,6 +92,17 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  let outputArr = [];
+  arr.forEach( num => {
+    if (num % 5 === 0 && num % 3 === 0){
+      outputArr.push('Fizz Buzz')
+    } else if (num % 5 === 0){
+      outputArr.push('Buzz')
+    } else if (num % 3 === 0){
+      outputArr.push('Fizz')
+    } else outputArr.push(num);
+  });
+  return(outputArr);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -118,7 +129,7 @@ xdescribe('Testing challenge 2', () => {
   });
 });
 
-describe('Testing challenge 3', () => {
+xdescribe('Testing challenge 3', () => {
   const inventory = [{ name: 'apples', available: true }, { name: 'pears', available: true }, { name: 'oranges', available: false }, { name: 'bananas', available: true }, { name: 'blueberries', available: false }];
 
   test('It should only add the available items to the list', () => {
@@ -127,7 +138,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
