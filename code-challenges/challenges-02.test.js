69758dcec77833e3aba2f98996c0839061c9513d
Klace Koch <koch.klace@gmail.com>
2021-01-07 14:36:16 -0800

    Challenge 2 done

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -26,6 +26,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let newString = str + ' The end.';
+  return newString;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -118,7 +120,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should append without modifying the oiginal', () => {
     const a = 'This is my story.';
     const b = appendTheEnd(a);
