56bffbc8ab078da61525f8f3a42c53743e63fc6b
Klace Koch <koch.klace@gmail.com>
2021-02-14 10:46:58 -0800

    remember to use the right character

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -32,7 +32,8 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 
 const addQuestion = (arr) => {
   // Solution code here...
-}
+  return(arr.map( str => str + '?'));
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
