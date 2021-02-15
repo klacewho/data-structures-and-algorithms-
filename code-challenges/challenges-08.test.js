5a0d634d23df1eb8254812903ff2f6283827f654
Klace Koch <koch.klace@gmail.com>
2021-02-15 13:54:38 -0800

    use regex to filter d

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -23,8 +23,7 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 
 const typeNum = (arr) => {
   // Solution code here...
-  let a = arr.filter(x => typeof x === 'number');
-  return(a);
+  return arr.filter(x => typeof x === 'number');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -37,6 +36,8 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  let a = arr.filter(x => /[d]/.test(x));
+  return(a);
 };
 
 /* ------------------------------------------------------------------------------------------------
