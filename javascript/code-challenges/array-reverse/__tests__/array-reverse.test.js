e9cd045805c4afc078ea74ab510e7694c92f3923
Klace Koch <koch.klace@gmail.com>
2021-02-20 22:51:42 -0800

    hey john said permission to plagiarize

--- javascript/code-challenges/array-reverse/__tests__/array-reverse.test.js
+++ javascript/code-challenges/array-reverse/__tests__/array-reverse.test.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const reverse = require('../array-reverse');
+const reverse = require('../array-reverse.js');
 
 describe('Array Reverse', () => {
-  it ('should reverse the dang ol array', () => {
-    let testArr = [1,2,3,4,5];
+
+  it('should reverse the array', () => {
+    let myArray = [1,2,3,4,5];
     let expected = [5,4,3,2,1];
-    reverse(testArr);
-    expected(testArr).toEqual(expected);
+    reverse(myArray);
+    expect(myArray).toEqual(expected);
   });
+
 });
--- javascript/code-challenges/array-reverse/array-reverse.js
+++ javascript/code-challenges/array-reverse/array-reverse.js
@@ -1,14 +1,15 @@
+
 'use strict';
 
-const reverse = (arr) => {
+const reverse = (list) => {
   let start = 0;
-  let end = arr.length -1;
-  while (start <= end){
-    let temp = arr[start];
-    arr[start] = arr[end];
-    arr.end = temp;
-    start ++;
-    end --;
+  let end = list.length - 1;
+  while (start <= end ) {
+    let temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
+    start++;
+    end--;
   }
 };
 
